7314ce8d1161e4c64b5c336ddcf8bbc8230fe774
Nông Quốc Khánh <56966634+hkgkhanh@users.noreply.github.com>
2021-06-18 15:39:46 +0700

    Update keyEvents.js

--- scripts/keyEvents.js
+++ scripts/keyEvents.js
@@ -29,74 +29,89 @@ document.onkeyup = function(e) {
 
 document.onkeydown = function(e) {
 	if (e.altKey && e.keyCode === 50) {
+		e.preventDefault();
 		document.getElementById("scramSelect").selectedIndex = 1;
 		generateScramble(document.getElementById('scramSelect').value);
 		saveSettings();
 
 	} else if (e.altKey && e.keyCode === 51) {
+		e.preventDefault();
 		document.getElementById("scramSelect").selectedIndex = 0;
 		generateScramble(document.getElementById('scramSelect').value);
 		saveSettings();
 
 	} else if (e.altKey && e.keyCode === 52) {
+		e.preventDefault();
 		document.getElementById("scramSelect").selectedIndex = 2;
 		generateScramble(document.getElementById('scramSelect').value);
 		saveSettings();
 		
 	} else if (e.altKey && e.keyCode === 53) {
+		e.preventDefault();
 		document.getElementById("scramSelect").selectedIndex = 3;
 		generateScramble(document.getElementById('scramSelect').value);
 		saveSettings();
 		
 	} else if (e.altKey && e.keyCode === 54) {
+		e.preventDefault();
 		document.getElementById("scramSelect").selectedIndex = 4;
 		generateScramble(document.getElementById('scramSelect').value);
 		saveSettings();
 		
 	} else if (e.altKey && e.keyCode === 55) {
+		e.preventDefault();
 		document.getElementById("scramSelect").selectedIndex = 5;
 		generateScramble(document.getElementById('scramSelect').value);
 		saveSettings();
 		
 	} else if (e.altKey && e.keyCode === 77) {
+		e.preventDefault();
 		document.getElementById("scramSelect").selectedIndex = 6;
 		generateScramble(document.getElementById('scramSelect').value);
 		saveSettings();
 		
 	} else if (e.altKey && e.keyCode === 80) {
+		e.preventDefault();
 		document.getElementById("scramSelect").selectedIndex = 7;
 		generateScramble(document.getElementById('scramSelect').value);
 		saveSettings();
 		
 	} else if (e.altKey && e.keyCode === 49) {
+		e.preventDefault();
 		document.getElementById("scramSelect").selectedIndex = 8;
 		generateScramble(document.getElementById('scramSelect').value);
 		saveSettings();
 		
 	} else if (e.altKey && e.keyCode === 83) {
+		e.preventDefault();
 		document.getElementById("scramSelect").selectedIndex = 9;
 		generateScramble(document.getElementById('scramSelect').value);
 		saveSettings();
 		
 	} else if (e.altKey && e.keyCode === 67) {
+		e.preventDefault();
 		document.getElementById("scramSelect").selectedIndex = 10;
 		generateScramble(document.getElementById('scramSelect').value);
 		saveSettings();
 		
 	} else if (e.altKey && e.keyCode === 79) {
+		e.preventDefault();
 		document.getElementById("scramSelect").selectedIndex = 11;
 		generateScramble(document.getElementById('scramSelect').value);
 		saveSettings();
 		
 	} else if (e.altKey && e.keyCode === 66) {
+		e.preventDefault();
 		document.getElementById("scramSelect").selectedIndex = 12;
 		generateScramble(document.getElementById('scramSelect').value);
 		saveSettings();
 		
 	} else if (e.altKey && e.keyCode === 76) {
+		e.preventDefault();
 		toggleTheme();
 		
 	} else if (e.keyCode === 13) {
+		e.preventDefault();
 		if (document.getElementById("timesListDiv").style.display !== "block" && document.getElementById("aboutDiv").style.display !== "block" ) {
 
 			if (document.getElementById("input").value !== "") {
@@ -108,27 +123,36 @@ document.onkeydown = function(e) {
 			
 		}
 	} else if (e.altKey && e.keyCode === 90) {
+		e.preventDefault();
 		delSolve();
 		
 	} else if (e.altKey && e.keyCode === 68) {
+		e.preventDefault();
 		resetSession();
 		
 	} else if (e.altKey && e.keyCode === 37) {
+		e.preventDefault();
 		getLastScramble();
 		
 	} else if (e.altKey && e.keyCode === 39) {
+		e.preventDefault();
 		generateScramble(document.getElementById('scramSelect').value);
 		
 	} else if (e.ctrlKey && e.keyCode === 49) {
+		e.preventDefault();
 		setOK();
 		
 	} else if (e.ctrlKey && e.keyCode === 50) {
+		e.preventDefault();
 		setP2();
 		
 	} else if (e.ctrlKey && e.keyCode === 51) {
+		e.preventDefault();
 		setDNF();
 		
 	} else if (e.altKey && e.keyCode === 73) {
+		e.preventDefault();
+		
 		var newCount = prompt("mean of X average(s) of 5: X =", avgCount);
 
 		if (newCount != null && parseInt(newCount) > 0) {
@@ -140,4 +164,3 @@ document.onkeydown = function(e) {
 		
 	}
 }
-
